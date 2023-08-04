c5602fc7fdcbdc6e3bd6243e5a2a601990ea8179
Donovan Raber <donovan.raber@outlook.com>
2023-08-03 18:33:38 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+var cats= ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+
+function appendCat(Broom) {
+    var newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat(Arnold) {
+    var newCats2 = ["Arnold", ...cats];
+    return newCats2;
+}
+
+function removeLastCat() {
+    var newCats3 = cats.slice(0, cats.length - 1);
+    return newCats3;
+}
+
+function removeFirstCat() {
+    var newCats4 = cats.slice(1);
+    return newCats4;
+}
